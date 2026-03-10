118de551d09d8f2269573b9ad16cd2425aff340b
Faiz G <faizmg52@gmail.com>
2026-03-11 03:13:05 +0530

    Added a greet method

--- Methods.java
+++ Methods.java
@@ -1,31 +1,17 @@
 public class Methods {
     public static void main(String[] args) {
                                             //  System.out.println(Math.min(2, 4));
-                                            //  System.out.println(Math.max(2, 4));
+        greet("Jack");                                    //  System.out.println(Math.max(2, 4));
                                             //  System.out.println(Math.random());
-      genRandNum();
+     
 
 
     }
 
-     public static void genRandNum() {
-        System.out.println((int)(Math.random()*6)+1);
+     public static void greet(String name) {
+        System.out.println("Hello "+ name);
         
      }
-    // public static void Greet() {
-    //     System.out.println("Hello");
-        
-    // }
-    // public static void Sum(int a1,int a2) {
-    //     System.out.println("Sum is:"+(a1+a2));
-                   
-    // } 
-    // public static void Table(int n) {
-    //     for (int i = 1; i < 10; i++) {
-    //         System.out.println(n+"*"+i+"="+(n*i));
-             //}
-    //     }
-        
-     // hello added new comments
+    
 }
 
