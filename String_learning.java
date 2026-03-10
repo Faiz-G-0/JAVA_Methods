9719a0630a18c4598f49cceb7b29dca46ac2033f
Faiz G <faizmg52@gmail.com>
2026-03-11 02:07:11 +0530

    Added String Fiels

---
+++ String_learning.java
@@ -0,0 +1,28 @@
+public class String_learning {
+    public static void main(String[] args) {
+      
+
+        String s1=new String("jacK");
+        String name="jack";
+        String name1="jack";
+        System.out.println(s1);
+                                                    // if(name==name1) {
+         if (name.equalsIgnoreCase(s1)) {
+            System.out.println("Both have same value");
+            
+         }                                          //     System.out.println("Both are same");  
+                                                    // }
+                                                    //  if(name==s1){
+                                                    //     System.out.println("BOth are same");
+                                                    //  }
+                                                    //  else{
+                                                    //     System.out.println("Both are not same");
+                                                    //  }
+                                                     /*
+                                                     the"==" thid opetstor only compare primitives data type's value 
+                                                     if you wanna ecompare string use "equale() and equaleignorecase()" methods
+                                                     */
+
+    }
+    
+}
