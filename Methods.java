462b97fb090ff43d76d2f650e92499aa44bace04
Faiz G <faizmg52@gmail.com>
2026-03-11 02:40:53 +0530

    Added methods file

---
+++ Methods.java
@@ -0,0 +1,31 @@
+public class Methods {
+    public static void main(String[] args) {
+                                            //  System.out.println(Math.min(2, 4));
+                                            //  System.out.println(Math.max(2, 4));
+                                            //  System.out.println(Math.random());
+      genRandNum();
+
+
+    }
+
+     public static void genRandNum() {
+        System.out.println((int)(Math.random()*6)+1);
+        
+     }
+    // public static void Greet() {
+    //     System.out.println("Hello");
+        
+    // }
+    // public static void Sum(int a1,int a2) {
+    //     System.out.println("Sum is:"+(a1+a2));
+                   
+    // } 
+    // public static void Table(int n) {
+    //     for (int i = 1; i < 10; i++) {
+    //         System.out.println(n+"*"+i+"="+(n*i));
+             //}
+    //     }
+        
+     
+}
+
